c8af07b50a1bc6bba41e17e1fb458f274deef8fd
matangub <Matangub@gmail.com>
2023-04-06 22:14:01 +0300

    implemented rrweb

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
+import './rrweb.min.js'
+
+// @ts-ignore
+console.log('rrweb', rrweb)
+
+let events: any = [];
+
+// @ts-ignore
+const stop = rrweb.record({
+  emit(event: any) {
+    // push event into the events array
+      events.push(event);
+  },
+});
+
+setInterval(() => {
+  console.log({events});
+}, 1000)
+
 export const add = (a: number, b: number) => {
   return a + b
 }
